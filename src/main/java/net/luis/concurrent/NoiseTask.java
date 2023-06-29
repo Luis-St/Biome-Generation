ac8db9b4c270755778b0c5e012c4a81e349c0945
Luis <luis@staudt-fridingen.de>
2023-06-29 21:27:14 +0200

    added NoiseTask class

---
+++ src/main/java/net/luis/concurrent/NoiseTask.java
@@ -0,0 +1,39 @@
+package net.luis.concurrent;
+
+import net.luis.utils.math.Mth;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public class NoiseTask {
+	
+	public NoiseTask(int threads, int size, NoiseGetter getter) {
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static interface NoiseGetter {
+		
+		double getValue(double x, double z);
+	}
+}
