5d6a5c8ccd994c90340cbec4d1ed99b45bb6a7a0
Luis <luis@staudt-fridingen.de>
2023-06-29 21:27:34 +0200

    added Test for split Rectangle

--- src/main/java/net/luis/run/ImageRun.java
+++ src/main/java/net/luis/run/ImageRun.java
@@ -19,17 +19,19 @@ public class ImageRun {
 	
 	// TODO: add windyness
 	
+	static {
+		LoggingUtils.initialize(LoggerConfiguration.DEFAULT);
+	}
+	
 	public static final DateFormat FORMAT = new SimpleDateFormat("mm:ss:SSS");
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public static void main(String[] args) throws IOException {
-		
-		long start = System.currentTimeMillis();
 		int size = Integer.parseInt(args[0]);
 		LOGGER.info("Started making noise images");
 		Thread temperature = new Thread(() -> {
 			try {
-				ImageIO.write(temperature(size), "png", new File(System.getProperty("user.home") + "\\Desktop\\temperature.png"));
+				ImageIO.write(temperature(size), "png", new File(System.getProperty("user.home") + "/Desktop/temperature.png"));
 				LOGGER.info("Made temperature noise image");
 			} catch (IOException exception) {
 				LOGGER.error("Could not make temperature noise image", exception);
@@ -37,7 +39,7 @@ public class ImageRun {
 		});
 		Thread humidity = new Thread(() -> {
 			try {
-				ImageIO.write(humidity(size), "png", new File(System.getProperty("user.home") + "\\Desktop\\humidity.png"));
+				ImageIO.write(humidity(size), "png", new File(System.getProperty("user.home") + "/Desktop/humidity.png"));
 				LOGGER.info("Made humidity noise image");
 			} catch (IOException exception) {
 				LOGGER.error("Could not make humidity noise image", exception);
@@ -45,7 +47,7 @@ public class ImageRun {
 		});
 		Thread continentalness = new Thread(() -> {
 			try {
-				ImageIO.write(continentalness(size), "png", new File(System.getProperty("user.home") + "\\Desktop\\continentalness.png"));
+				ImageIO.write(continentalness(size), "png", new File(System.getProperty("user.home") + "/Desktop/continentalness.png"));
 				LOGGER.info("Made continental noise image");
 			} catch (IOException exception) {
 				LOGGER.error("Could not make continental noise image", exception);
@@ -53,7 +55,7 @@ public class ImageRun {
 		});
 		Thread erosion = new Thread(() -> {
 			try {
-				ImageIO.write(erosion(size), "png", new File(System.getProperty("user.home") + "\\Desktop\\erosion.png"));
+				ImageIO.write(erosion(size), "png", new File(System.getProperty("user.home") + "/Desktop/erosion.png"));
 				LOGGER.info("Made erosion noise image");
 			} catch (IOException exception) {
 				LOGGER.error("Could not make erosion noise image", exception);
@@ -65,7 +67,7 @@ public class ImageRun {
 		erosion.start();
 	}
 	
-	protected static BufferedImage temperature(int size) {
+	private static BufferedImage temperature(int size) {
 		BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D graphics = image.createGraphics();
 		for (int x = 0; x <= size; x++) {
@@ -77,7 +79,7 @@ public class ImageRun {
 		return image;
 	}
 	
-	protected static BufferedImage humidity(int size) {
+	private static BufferedImage humidity(int size) {
 		BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D graphics = image.createGraphics();
 		for (int x = 0; x <= size; x++) {
@@ -89,7 +91,7 @@ public class ImageRun {
 		return image;
 	}
 	
-	protected static BufferedImage continentalness(int size) {
+	private static BufferedImage continentalness(int size) {
 		BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D graphics = image.createGraphics();
 		for (int x = 0; x <= size; x++) {
@@ -101,7 +103,7 @@ public class ImageRun {
 		return image;
 	}
 	
-	protected static BufferedImage erosion(int size) {
+	private static BufferedImage erosion(int size) {
 		BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D graphics = image.createGraphics();
 		for (int x = 0; x <= size; x++) {
--- src/main/java/net/luis/run/TestRun.java
+++ src/main/java/net/luis/run/TestRun.java
@@ -1,30 +1,91 @@
 package net.luis.run;
 
-import net.luis.noise.Noises;
+import net.luis.utils.logging.LoggerConfiguration;
+import net.luis.utils.logging.LoggingUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class TestRun {
 	
+	static {
+		LoggingUtils.initialize(LoggerConfiguration.DEFAULT);
+	}
+	
 	public static final Logger LOGGER = LogManager.getLogger();
 	
 	public static void main(String[] args) {
-//		Noise noise = PerlinNoise.create(new WorldgenRandom(10), IntStream.rangeClosed(-3, 0));
-//		double min = 0.0;
-//		double max = 0.0;
-//		for (int x = 0; x <= 100000; x++) {
-//			for (int z = 0; z <= 100000; z++) {
-//				double value = noise.getValue(x * 0.0625, z * 0.0625);
-//				min = Math.min(min, value);
-//				max = Math.max(max, value);
-//			}
-//		}
-//		LOGGER.info("PerlinNoise min: {}", min);
-//		LOGGER.info("PerlinNoise max: {}", max);
-//		LOGGER.info("");
-		LOGGER.info("TEMPERATURE, min: {}, max: {}", Noises.TEMPERATURE.min(), Noises.TEMPERATURE.max());
+/*		Noise noise = PerlinNoise.create(new WorldgenRandom(10), IntStream.rangeClosed(-3, 0));
+		double min = 0.0;
+		double max = 0.0;
+		for (int x = 0; x <= 100000; x++) {
+			for (int z = 0; z <= 100000; z++) {
+				double value = noise.getValue(x * 0.0625, z * 0.0625);
+				min = Math.min(min, value);
+				max = Math.max(max, value);
+			}
+		}
+		LOGGER.info("PerlinNoise min: {}", min);
+		LOGGER.info("PerlinNoise max: {}", max);
+		LOGGER.info("");*/
+/*		LOGGER.info("TEMPERATURE, min: {}, max: {}", Noises.TEMPERATURE.min(), Noises.TEMPERATURE.max());
 		LOGGER.info("HUMIDITY, min: {}, max: {}", Noises.HUMIDITY.min(), Noises.HUMIDITY.max());
 		LOGGER.info("CONTINENTALNESS, min: {}, max: {}", Noises.CONTINENTALNESS.min(), Noises.CONTINENTALNESS.max());
-		LOGGER.info("EROSION, min: {}, max: {}", Noises.EROSION.min(), Noises.EROSION.max());
+		LOGGER.info("EROSION, min: {}, max: {}", Noises.EROSION.min(), Noises.EROSION.max());*/
+		
+		int threads = 10;
+		int size = 10;
+		
+		divideRectangle(size, size, threads);
+		
+		
+	}
+	
+	public static void divideRectangle(int width, int height, int parts) {
+		if (parts <= 0) {
+			LOGGER.warn("Invalid parts count. Please provide a positive value.");
+			return;
+		}
+		
+		if (width <= 0 || height <= 0) {
+			LOGGER.warn("Invalid rectangle dimensions. Please provide positive values for width and height.");
+			return;
+		}
+		
+		int rows = 1;
+		int cols = parts;
+		
+		int minDifference = Math.abs(rows - cols);
+		
+		for (int i = 2; i <= Math.sqrt(parts); i++) {
+			if (parts % i == 0) {
+				int currentCols = parts / i;
+				int difference = Math.abs(i - currentCols);
+				
+				if (difference < minDifference) {
+					minDifference = difference;
+					rows = i;
+					cols = currentCols;
+				}
+			}
+		}
+		
+		int partWidth = width / cols;
+		int partHeight = height / rows;
+		
+		LOGGER.info("Rectangle dimensions: width={}, height={}", width, height);
+		LOGGER.info("Parts count: {}", parts);
+		System.out.println();
+		
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				int x = j * partWidth;
+				int y = i * partHeight;
+				
+				LOGGER.info("Part {}", (i * cols + j + 1));
+				LOGGER.info("Top: ({}, {})", x + 1, y + 1);
+				LOGGER.info("Bottom: ({}, {})", partWidth + 1, partHeight + 1);
+				System.out.println();
+			}
+		}
 	}
 }
